44686fb91c6b3516cd7a59430a157d50b6418003
seifo <mettaiseifedin@gmail.com>
2024-02-25 15:20:34 -0800

    Exo 5 Done Version Beta

---
+++ src/test/java/FactorialTest.java
@@ -0,0 +1,29 @@
+import org.example.Factorial;
+import org.example.Fibonacci;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class FactorialTest {
+    @Test
+    public void FactorialTestShouldReturnIllegalArgumentExceptionWhenNumberIsNegative(){
+        Assertions.assertThrows(IllegalArgumentException.class,()-> Factorial.factorial(-1));
+    }
+    @Test
+    public void FactorialTestShouldReturn1WhenNumberIsEqual0(){
+        int result = Factorial.factorial(0);
+        Assertions.assertEquals(1,result);
+
+    }
+    @Test
+    public void FactorialTestShouldReturn1WhenNumberIsEqual1(){
+        int result = Factorial.factorial(1);
+        Assertions.assertEquals(1,result);
+
+    }
+    @Test
+    public void FactorialTestShouldReturnTheRightFactorialWhenNumberIsEqualGreaterThen1(){
+        int result = Factorial.factorial(2);
+        Assertions.assertEquals(2,result);
+
+    }
+}
