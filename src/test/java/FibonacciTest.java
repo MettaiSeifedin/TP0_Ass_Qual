08a4c2530fa89271a184edf542200758c044dd6c
seifo <mettaiseifedin@gmail.com>
2024-02-26 01:04:11 -0800

    Tp0 Done

--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -3,25 +3,25 @@ import org.junit.Test;
     import org.junit.jupiter.api.Assertions;
     public class FibonacciTest {
     @Test
-    public void FibonacciTestShouldReturnExceptionWhenNombreIsNegative()
+    public void FibonacciTestShouldReturnExceptionWhenNumberIsNegative()
     {
         Assertions.assertThrows(IllegalArgumentException.class,()->Fibonacci.fibonacci(-1));
     }
 
     @Test
-    public void FibonacciTestShouldRerturn0whenNombreis0(){
+    public void FibonacciTestShouldRerturn0whenNumberIs0(){
         int result = Fibonacci.fibonacci(0);
         Assertions.assertEquals(0,result);
 
     }
         @Test
-        public void FibonacciTestShouldRerturn1whenNombreis1(){
+        public void FibonacciTestShouldRerturn1whenNumberIs1(){
             int result = Fibonacci.fibonacci(1);
             Assertions.assertEquals(1,result);
 
         }
         @Test
-        public void FibonacciTestShouldRerturnTheRightResultwhenNombreisbigerthan1(){
+        public void FibonacciTestShouldRerturnTheRightResultwhenNumberIsGreaterthen1(){
             int result = Fibonacci.fibonacci(2);
             Assertions.assertEquals(1,result);
 
